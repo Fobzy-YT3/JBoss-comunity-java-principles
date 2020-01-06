1a148ad26b421cda4c30eb424096dcbff58ad98d
Fobzy-YT3 <59029280+Fobzy-YT3@users.noreply.github.com>
2020-01-06 10:06:00 -0600

    Add files via upload

---
+++ abstraction.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class abstraction here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+interface abstraction
+{
+    final String x = "From one class... ";
+    
+    void display();
+}
---
+++ abstractionTest.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class abstractionTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */ 
+public class abstractionTest implements abstraction 
+{
+    public void display()
+    {
+        System.out.print("to the next!");
+    }
+    
+    public static void main(String[] args)
+    {
+        abstractionTest a = new abstractionTest();
+        System.out.print(x);
+        a.display();
+        
+    }
+}
